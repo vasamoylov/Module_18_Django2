edb077a73691599dd5fe4b1473c97729fed4fb23
vasamoylov <vfea-vitalik@mail.ru>
2024-11-17 22:31:21 +0300

    Add files via upload

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def wellcome(request):
+    return render(request, 'main_page.html')
+
+
+def games(request):
+    return render(request, 'games.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
