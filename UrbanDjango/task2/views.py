7db4ee7e750ca7cff55b5c7d83aeb9a1dc34ccd7
vasamoylov <vfea-vitalik@mail.ru>
2024-11-18 23:41:16 +0300

    Add files via upload

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -3,13 +3,4 @@ from django.views.generic import TemplateView
 
 
 # Create your views here.
-def wellcome(request):
-    return render(request, 'main_page.html')
 
-
-def games(request):
-    return render(request, 'games.html')
-
-
-def cart(request):
-    return render(request, 'cart.html')
--- UrbanDjango/task4/views.py
+++ UrbanDjango/task4/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def wellcome(request):
+    return render(request, 'main_page.html')
+
+
+def games(request):
+    games_list = ['GTA V', 'NFS', 'FIFA']
+    context = {'games_list': games_list}
+    return render(request, 'games.html', context)
+
+
+def cart(request):
+    return render(request, 'cart.html')
